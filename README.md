ecc5048725cfbf823dce0df60d2537bd9a63830a
cyc <wangyi1113795069@163.com>
2024-12-16 22:59:32 +0800

    third commit

--- README.md
+++ README.md
@@ -9,5 +9,15 @@ This is a fuzzy PID controller implementation in Cpp
 其中，==rules.h==的创建是为了更好的分类，便于读者集中注意力于控制器的实现
 其次，==CMakeLists.txt==文件用于编译项目，当该项目是在ros的功能包中使用，因此若直接下载无法直接编译
 
-## Usage
-有待继续更新
+## indroduction about fuzzy PID controller
+本人深感网络上关于模糊PID控制器的资料较少，因此在此对模糊PID控制器的原理及其实现进行简要介绍。
+概言之模糊PID可由如此形容：
+input->fuzzy->get the membership->rules->output
+其中，input为输入，fuzzy为模糊处理，membership为模糊集，rules为规则，output为输出。
+- 首先，input包含误差error和误差变化率error_dot。
+- 然后，fuzzy处理input，将其映射到模糊集membership中。
+  - 简单来说，input可以同时属于多个模糊集(如PB、PM，可以同时隶属于几个模糊集可自行设定)
+  - fuzzy处理就是将input映射到membership中，计算出input隶属于各个模糊集的程度
+- 接着，rules根据membership计算输出，也称为defuzzy处理。
+  - 简单来说，rules根据membership计算出输出。
+- 最后，output为PID控制器的delta(增量)输出。
--- fuzzy_PID.h
+++ fuzzy_PID.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <cmath>
 #include "rules.h"
-const double e_norm = 5; // 误差模糊化范围
+const double e_norm = 5;  // 误差模糊化范围
 const double de_norm = 5; // 误差导数模糊化范围
 namespace fuzzy_pid
 {
@@ -11,32 +11,39 @@ namespace fuzzy_pid
     {
     public:
         FuzzyPID(double kp, double ki, double kd)
-            : kp(kp), ki(ki), kd(kd){}
+            : kp(kp), ki(ki), kd(kd) {}
 
         void compute(double setpoint, double pv, double dt)
         {
             error = setpoint - pv;
             integral += error;
             error_dot = (error - prev_error) / dt;
+            prev_error = error;
             membership.empty();
             // 模糊化输入
-            Get_membership(error/e_norm,e_range);
-            Get_membership(error_dot/de_norm,de_range);
+            Get_membership(error / e_norm, e_range);
+            Get_membership(error_dot / de_norm, de_range);
             // 得到输入对应的隶属度函数
             // 下结合规则得到输出解模糊化
-            delta_kd=0;
-            delta_ki=0;
-            delta_kp=0;
+            delta_kd = 0;
+            delta_ki = 0;
+            delta_kp = 0;
             defuzz_method();
             kp += delta_kp;
             ki += delta_ki;
             kd += delta_kd;
+            kp = kp > kp_max ? kp_max : kp;
+            ki = ki > ki_max ? ki_max : ki;
+            kd = kd > kd_max ? kd_max : kd;
+            kp = kp < kp_min ? kp_min : kp;
         }
 
     private:
-        
         double kp, ki, kd;
+        double kp_min, ki_min, kd_min;
+        double kp_max, ki_max, kd_max;
         double delta_kp, delta_ki, delta_kd;
+        double delta_kp_max, delta_ki_max, delta_kd_max;
         std::vector<double> membership;
         std::vector<int> index;
         double error;
@@ -47,10 +54,8 @@ namespace fuzzy_pid
         // 这里采用重心法解模糊
         void defuzz_method();
         // 输入模糊化
-        void Get_membership(double input, float* range);
+        void Get_membership(double input, float *range);
         // 三角隶属度函数
         double trimf(double input, double min, double max);
-        
-         
     };
 } // namespace fuzzy_pid
